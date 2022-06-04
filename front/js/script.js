9a47972fb7a21d79e9418fb447f0be16f71d6460
MaG <mmaxou@msn.com>
2022-06-05 00:43:45 +0200

    innerHTML

--- front/js/script.js
+++ front/js/script.js
@@ -8,29 +8,33 @@ fetch('http://localhost:3000/api/products')
             //Etape 1 je me place dans la section HTML items - nom: section
             const section = document.getElementById('items');
             console.log(section);
-            // Etape 2 avec const couch, je crée une sélection d'éléments dans la liste
-            const couch = products;
-            console.log(couch);
-            // Etape 3 je crée mes variables pour la future vignette de chaque produit
-            let ref = `${couch["_id"]}`;
-            let name = `${couch["name"]}`;
-            let image = `${couch["imageUrl"]}`;
-            let description = `${couch["description"]}`;
-            // Etape 4 en m'aidant du tableau l'assignation est automatique dans le HTML
-
-            for (let i = 0; i < couch.length; i++) {
-                  couch[i];
-                  couch.innerHTML =
+            // Etape 2 je crée mes variables pour la future vignette de chaque produit
+            let kName = `${products["name"]}`;
+            let kImage = `${products["imageUrl"]}`;
+            let kDescription = `${products["description"]}`;
+            // Etape 3 en m'aidant du tableau l'assignation est automatique dans le HTML
+            for (let i = 0; i < products.length; i++) {
+                  products[i];
+                  // Etape 4 en fait c'est le a href que je devrais viser pour placer les résultats dans les vignettes
+                  section.innerHTML =
                         `<section id="items" class="items">
-                        <a href=${ref}>
+                        <a href="./product.html?id=42">
                               < article >
-                                    <img src= ${image} alt="">
-                                    <h3 class="productName">${name}</h3>
-                                    <p class="productDescription">${description}</p>
+                                    <img src= ${kImage} alt="">
+                                    <h3 class="productName">${kName}</h3>
+                                    <p class="productDescription">${kDescription}</p>
                               </article>
-                        </section>`
-                  console.log(couch.innerHTML);
-                  // bonne nouvelle répétition 8 fois. Mauvaise nouvelle c'est indéfini
+                        </a></section>
+                              <section id="items" class="items">
+                        <a href="./product.html?id=42">
+                              < article >
+                                    <img src= ${kImage} alt="">
+                                    <h3 class="productName">${kName}</h3>
+                                    <p class="productDescription">${kDescription}</p>
+                              </article>
+                        </a></section>`
+                  console.log(section.innerHTML);
+                  // bonne nouvelle répétition 8 fois dans la console. Mauvaise nouvelle c'est indéfini
 
             }
       });
